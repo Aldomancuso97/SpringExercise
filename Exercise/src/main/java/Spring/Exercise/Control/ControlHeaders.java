de7551baa69b971d7b161adc0ced3ea26fadfcee
Aldo Mancuso <mancusoaldo8 @gmail.com>
2023-03-12 16:53:55 +0100

    Exercise Header

---
+++ Exercise/src/main/java/Spring/Exercise/Control/ControlHeaders.java
@@ -0,0 +1,23 @@
+package Spring.Exercise.Control;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetSocketAddress;
+import java.net.http.HttpHeaders;
+
+@RestController
+public class ControlHeaders {
+
+
+   /* @GetMapping("/headers")
+    public String getHeader(@RequestHeader HttpHeaders headers) {
+        //prendo oggetto Host
+        InetSocketAddress host = headers.getHost();
+        return ("The host is: " + host.getHostName()
+                + " and the port is: " + host.getPort());
+    }*/
+}
+
